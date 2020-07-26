aa45b061d7215f60b19117b224a4523dee861c50
Dan Weikart <danmweikart@gmail.com>
2020-07-26 14:29:57 -0700

    Updates

---
+++ precache-manifest.8d37c5e5c28f46d7a87bccec6d2815cf.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7d2a94570c897020cda5e34e62f659",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3ea917337bf0d64ff1e3",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f65d8dcfe70619585957",
+    "url": "/portfolio/static/css/main.63ed21db.chunk.css"
+  },
+  {
+    "revision": "3ea917337bf0d64ff1e3",
+    "url": "/portfolio/static/js/2.439fb60e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.439fb60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65d8dcfe70619585957",
+    "url": "/portfolio/static/js/main.983f91cf.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "62923853041ef42d8c685e56575aedbd",
+    "url": "/portfolio/static/media/Dan_Clipper_rev.62923853.gif"
+  },
+  {
+    "revision": "0f41465ff0d99f2146d47d785bd5d083",
+    "url": "/portfolio/static/media/Dan_Clipper_twoFirst.0f41465f.gif"
+  },
+  {
+    "revision": "2a503349b198ac67b5b4953754997d35",
+    "url": "/portfolio/static/media/Dan_Weikart_Resume_6-25-2020.2a503349.pdf"
+  },
+  {
+    "revision": "9317bab3e3155efe361ad5c7f42fe83d",
+    "url": "/portfolio/static/media/aqir_home_screen_4-3.9317bab3.png"
+  },
+  {
+    "revision": "d958a863c0c23366722214683717604d",
+    "url": "/portfolio/static/media/aqir_home_screen_wide.d958a863.png"
+  },
+  {
+    "revision": "1686837a95d9c5068c1a451086858d5a",
+    "url": "/portfolio/static/media/burger_4-3.1686837a.png"
+  },
+  {
+    "revision": "6c0680e1bbc96d397544588859185eb6",
+    "url": "/portfolio/static/media/burger_wide.6c0680e1.png"
+  },
+  {
+    "revision": "0bbfb40ede0546c37b7ec476b972741d",
+    "url": "/portfolio/static/media/dayPlanner_4-3.0bbfb40e.png"
+  },
+  {
+    "revision": "81b07e5660f8575cc4ade15a0c658da1",
+    "url": "/portfolio/static/media/dayPlanner_wide.81b07e56.png"
+  },
+  {
+    "revision": "1f264ccbf60104b9dad642ae71a953bd",
+    "url": "/portfolio/static/media/employeeRoster_4-3.1f264ccb.png"
+  },
+  {
+    "revision": "f7c8ce0b2b7501825c1cfed59f6e740b",
+    "url": "/portfolio/static/media/employeeRoster_wide.f7c8ce0b.png"
+  },
+  {
+    "revision": "120233615ef536d1f3a07ceae07c0bd4",
+    "url": "/portfolio/static/media/fearSetting_4-3.12023361.png"
+  },
+  {
+    "revision": "0982d28e5ef605bb9960bc0d5acd66a6",
+    "url": "/portfolio/static/media/fearSetting_Home_wide.0982d28e.png"
+  },
+  {
+    "revision": "9c8ea007d69c2233d226ff77ac5f11fc",
+    "url": "/portfolio/static/media/weatherDashboard_4-3.9c8ea007.png"
+  },
+  {
+    "revision": "61c0facf2b3a39466c30cc3b648f1fd5",
+    "url": "/portfolio/static/media/weatherDashboard_wide.61c0facf.png"
+  }
+]);
